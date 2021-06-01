669c3bbf3e3b76b868805c15865c9a27b30bd368
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-06-01 11:20:58 +0200

    assertFalse value

--- HealthyCoderApp/src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ HealthyCoderApp/src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -19,4 +19,18 @@ class BMICalculatorTest {
 		assertTrue(isRecomended);
 	}
 
+	
+	@Test
+	void return_false_if_diet_is_not_recomended() {
+//		given
+		double weight = 70.0;
+		double height = 1.72;
+		
+//		when
+		boolean isRecomended = BMICalculator.isDietRecommended(weight, height);
+		
+//		then
+		assertFalse(isRecomended);
+		
+	}
 }
